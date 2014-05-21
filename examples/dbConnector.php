e27875981e12794282f857062db2fe8c7018d468
Scott Burton <scotthburton@gmail.com>
2014-05-21 13:43:55 -0600

    cleaned up debugging code

--- examples/dbConnector.php
+++ examples/dbConnector.php
@@ -14,13 +14,13 @@ function loadDatabase()
 	if ($openShiftVar === null || $openShiftVar == "")
 	{
 		// Not in the openshift environment
-		echo "Using local credentials: ";
+		//echo "Using local credentials: ";
 		require("setLocalDatabaseCredentials.php");
 	}
 	else
 	{
 		// In the openshift environment
-		echo "Using openshift credentials: ";
+		//echo "Using openshift credentials: ";
 
 		$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
@@ -28,7 +28,7 @@ function loadDatabase()
 		$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 	}
 
-	echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+	//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 
 
 	$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
--- examples/openShiftPdoExample.php
+++ examples/openShiftPdoExample.php
@@ -6,14 +6,11 @@ require("dbConnector.php");
 $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$db = new PDO("mysql:host=$host:$port;dbname=testdb", $user, $password);
 */
-
-//$user = "php";
-//$password = "php-pass";
 
 try
 {
-//   $db = new PDO("mysql:host=$host:$port;dbname=testdb", $user, $password);
 
    $db = loadDatabase();
 
@@ -21,30 +18,6 @@ try
    {
       echo "id: " . $row['id'] . " userName: " . $row['userName'] . "<br />";
    }
-
-/*
-   foreach ($db->query("SELECT username, password FROM user") as $row)
-   {
-      echo "user: " . $row['username'] . " password: " . $row['password'] . "<br />";
-   }
-
-   $statement = $db->query("SELECT username, password FROM user");
-
-   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-   {
-      echo "user: " . $row['username'] . " password: " . $row['password'] . "<br />";
-   }
-
-   $statement = $db->query("SELECT username, password FROM user");
-   $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-   var_dump($results);
-
-   foreach ($results as $row)
-   {
-      echo "user: " . $row['username'] . " password: " . $row['password'] . "<br />";
-   }
-   */
-
 }
 catch (PDOException $ex)
 {
